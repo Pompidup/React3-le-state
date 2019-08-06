568acac547ce27131d95d32b542277248ca5d3a7
Pompidup <julienauvray28@gmail.com>
2019-08-06 22:58:05 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Quote from "./Quote";
+import Lamp from './Lamp';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+       working: false
+    };
+};
+
+_working = () => {
+  this.setState({working: !this.state.working});
+};
+
+  render() {
+
+    let classLogo = 'App-logo';
+
+    if (this.state.working) {
+      classLogo = 'App-logo2';
+    }
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className={classLogo} alt="logo" />
+          <h1 className="App-title">Simpsons Quotes</h1>
+        </header>
+        <button 
+          onClick={this._working}>
+            Go works
+        </button>
+        <Lamp on />
+        <Lamp />
+        <Quote
+          quote="I believe the children are the future... Unless we stop them now!"
+          character="Homer Simpson"
+          image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939"
+        />
+        <Quote
+          quote="Me fail English? That's unpossible"
+          character="Ralph Wiggum"
+          image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FRalphWiggum.png?1497567511523"
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
